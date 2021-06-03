a12cb00a2aae582eccc46b2e1ae738bf63a1c59d
Jake Schaffer <imjakedev@gmail.com>
2021-06-03 12:57:57 -0500

    fixed some auto formatting issues and added files for a bottom nav tmeplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
   env: {
+    browser: true,
     es2021: true,
+    "jest/globals": true,
     node: true,
+    "react-native/react-native": true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  plugins: ["react"],
   rules: {
+    "react/prop-types": [0, {}], // this is dumb, why is this still recommended
+    "react/no-unescaped-entities": [0, {}],
+    "no-unused-vars": [1, {}],
+    quotes: ["error", "double"],
   },
 };
