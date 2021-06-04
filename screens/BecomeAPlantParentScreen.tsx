31fa2ae0e3f5097309414270bf8323a0084f31f0
Jake Schaffer <imjakedev@gmail.com>
2021-06-04 16:40:05 -0500

    added a react-hook-form to screen

--- screens/BecomeAPlantParentScreen.tsx
+++ screens/BecomeAPlantParentScreen.tsx
@@ -1,10 +1,50 @@
 import * as React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
 export default function BecomeAPlantParentScreen(params:{}) {
+  const { control, handleSubmit, formState: { errors } } = useForm();
+
   return (
-    <View/>
-  )
+    <View style={styles.container}>
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value }}) => (
+          <TextInput
+            style={{paddingHorizontal: 20, borderWidth: 1, paddingVertical: 8}}
+            onBlur={onBlur}
+            onChangeText={value => onChange(value)}
+            value={value}
+          />
+        )}
+        name="firstName"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      {errors.firstName && <Text>First Name is required.</Text>}
+
+      <Controller
+        control={control}
+        render={({ field: { onChange, onBlur, value }}) => (
+          <TextInput
+            style={{paddingHorizontal: 20, borderWidth: 1, paddingVertical: 8}}
+            onBlur={onBlur}
+            onChangeText={value => onChange(value)}
+            value={value}
+          />
+        )}
+        name="lastName"
+        rules={{ required: true, minLength: 8}}
+        defaultValue=""
+      />
+
+      {errors.lastName?.type === "required" && <Text>Last Name is required.</Text>}
+
+      {errors.lastName?.type === "minLength" && <Text>Minimum 8 characters are required</Text>}
+
+      <Button title="Submit" onPress={handleSubmit((data) => console.log(data))} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -14,15 +54,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     paddingLeft: 10,
     paddingRight: 10,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 2,
-    width: "80%",
-  },
+  }
 });
