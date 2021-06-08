08336f8e25b20ccb111710396ce387a4ba50f699
Jake Schaffer <imjakedev@gmail.com>
2021-06-08 11:30:39 -0500

    starting on Apollo CLient setup

--- App.tsx
+++ App.tsx
@@ -2,10 +2,21 @@ import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { PROUD_PLANT_PARENT_ENDPOINT, X_HASURA_ADMIN_SECRET } from "@env";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
+
+const client = new ApolloClient({
+  uri: `${PROUD_PLANT_PARENT_ENDPOINT}`,
+  cache: new InMemoryCache(),
+  headers: {
+    "content-type": "application/json",
+    "x-hasura-admin-secret": `${X_HASURA_ADMIN_SECRET}`,
+  },
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -16,8 +27,10 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </ApolloProvider>
       </SafeAreaProvider>
     );
   }
