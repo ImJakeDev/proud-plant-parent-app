a66caf4a439c442924f8a2361a16740dcc1df913
Jake Schaffer <imjakedev@gmail.com>
2021-06-08 12:06:16 -0500

    built a graphql mutation and it worked

--- components/forms/ParentForm.tsx
+++ components/forms/ParentForm.tsx
@@ -1,16 +1,33 @@
-import React, { useRef } from "react";
+import * as React from "react";
 import { FormProvider, useForm } from "react-hook-form";
 import { View } from "react-native";
+import { gql, useMutation } from '@apollo/client';
 
 import { FormInput } from "../react-hook-forms/FormInput";
 import Button from "../Button";
 
+const ADD_PLANT_PARENT = gql`
+  mutation ($firstname: String!, $lastname: String!, $nickname: String! ) {
+    insert_plantparent(objects: {firstname: $firstname, lastname: $lastname, nickname: $nickname}) {
+      returning {
+        firstname
+        lastname
+        nickname
+        plantparentid
+        timeofparenthood
+      }
+    }
+  }
+`;
+
 export default function ParentForm() {
   const formMethods = useForm();
-  const passwordRef = useRef();
+
+  const [addPlantParent] = useMutation(ADD_PLANT_PARENT);
 
   const onSubmit = (form: any) => {
     console.log(form);
+    addPlantParent({ variables: { firstname: form.firstname, lastname: form.lastname, nickname: form.nickname } });
   };
 
   const onErrors = (errors: {}) => {
@@ -20,24 +37,6 @@ export default function ParentForm() {
   return (
     <View>
       <FormProvider {...formMethods}>
-        {/* <FormInput
-          name="UserName"
-          label="Username"
-          rules={{ required: "Username is required!" }}
-          returnKeyType="next"
-        />
-        <FormInput
-          name="Password"
-          label="Password"
-          rules={{
-            required: "Password is required!",
-            minLength: {
-              message: "Use at least 10 characters.",
-              value: 10,
-            },
-          }}
-          ref={passwordRef}
-        /> */}
         <FormInput
           name="firstname"
           label="First Name"
